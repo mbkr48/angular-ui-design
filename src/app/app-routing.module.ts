d6b0610de1c94f432027b11512030c7107b29147
Muhammad Abubakar <muhammad.abubakar@reolustech.net>
2022-01-26 15:37:56 +0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { MainComponent } from './components/main/main.component';
+import { ClientComponent } from './components/client/client.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AchievementComponent } from './components/achievement/achievement.component';
+
+const routes: Routes = [
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "portfolio",
+    component: PortfolioComponent
+  },
+  {
+    path: "testimonial",
+    component: TestimonialComponent
+  },
+  {
+    path: "client",
+    component: ClientComponent
+  },
+  {
+    path: "services",
+    component: ServicesComponent
+  },
+  {
+    path: "achievement",
+    component: AchievementComponent
+  },
+  {
+    path: "main",
+    component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ClientComponent } from './components/client/client.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AchievementComponent } from './components/achievement/achievement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    TestimonialComponent,
+    MainComponent,
+    ClientComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    AchievementComponent
   ],
   imports: [
     BrowserModule,
